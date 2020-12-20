5dd33534ee1ce1bb7aa894d3fcd03c9ee20fc82f
alex <bigbadmath@gmail.com>
2020-12-20 13:54:06 -0500

    a couple working faker examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use fake::{Dummy, Fake, Faker};
+// using `faker` module with locales
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use rand;
+
+#[derive(Debug, Dummy)]
+pub struct OrderRecord {
+    #[dummy(faker = "1000..2000")]
+    order_id: usize,
+    customer: String,
+    paid: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    for _ in 0..100 {
+        let f: OrderRecord = Faker.fake();
+        println!("{:?}", f);
+    }
+    let name_vec = fake::vec![String as Name(EN); 4, 3..5, 2];
+    println!("random nested vec {:?}", name_vec)
 }
